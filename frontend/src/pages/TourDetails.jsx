6fc047077b153dfd4cb3fdd817c3e7046399ebc2
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-01-08 13:13:55 +0200

    update TourDetails

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -3,6 +3,7 @@ import '../styles/tour-details.css'
 import { Container, Row, Col, Form, ListGroup } from 'reactstrap'
 import { useParams } from 'react-router-dom'
 import toutData from '../assets/data/tours'
+import calculateAvgRating from '../utils/avgRating'
 
 const TourDetails = () => {
   const { id } = useParams()
@@ -11,8 +12,19 @@ const TourDetails = () => {
   const tour = toutData.find((tour) => tour.id == id)
 
   // distracture properties from tour object
-  const { photo, title, desc, price, reviews, city, distance, maxGroupSize } =
-    tour
+  const {
+    photo,
+    title,
+    desc,
+    price,
+    address,
+    reviews,
+    city,
+    distance,
+    maxGroupSize,
+  } = tour
+
+  const { totalRating, avgRating } = calculateAvgRating(reviews)
 
   return (
     <>
@@ -25,6 +37,21 @@ const TourDetails = () => {
 
                 <div className="tour__info">
                   <h2>{title}</h2>
+
+                  <div className="d-flex align-items-center gap-5">
+                    <span className="tour__rating d-flex align-items-center gap-1">
+                      <i
+                        class="ri-star-s-fill"
+                        style={{ color: 'var(--secondary-color)' }}
+                      ></i>
+                      {calculateAvgRating == 0 ? null : avgRating}
+                      {totalRating == 0 ? (
+                        'Not rated'
+                      ) : (
+                        <span>({reviews.length})</span>
+                      )}
+                    </span>
+                  </div>
                 </div>
               </div>
             </Col>
