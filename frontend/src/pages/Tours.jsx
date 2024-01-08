9d8a672013e989e892c86c93484266c885d1558e
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-01-08 10:58:47 +0200

    add other section

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import CommonSection from '../shared/CommonSection'
+
+import '../styles/tour.css'
+import tourData from '../assets/data/tours'
+import TourCard from './../shared/TourCard'
+import SearchBar from './../shared/SearchBar'
+import Newsletter from './../shared/Newsletter'
+import { Container, Row, Col } from 'reactstrap'
+import {} from 'reactstrap'
 
 const Tours = () => {
-  return <div>Tours</div>
-};
 
-export default Tours;
+  const [pageCount, setPageCount] = useState(0)
+  const [page, setPage] = useState(0)
+
+  useEffect(()=>{
+
+    const pages = Math.ceil(5/ 8); // later we will use backend data count
+    setPageCount(pages);
+
+  },[page]);
+
+  return (
+    <>
+      <CommonSection title={'All Tour'} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" className="mb-4" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))}
+
+            <Col lg="12">
+              <div className="pagination d-flex align-items-center
+              justify-content-center mt-4 gap-3">
+                {[...Array(pageCount)]}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  )
+}
+
+export default Tours
