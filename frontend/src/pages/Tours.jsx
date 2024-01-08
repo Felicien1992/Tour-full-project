e8fbb10c73d5193fbe17302aa802a29cac6ccd2c
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-01-08 11:18:42 +0200

    add pagination css

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -43,7 +43,9 @@ const Tours = () => {
               <div className="pagination d-flex align-items-center
               justify-content-center mt-4 gap-3">
                 {[...Array(pageCount).keys()].map(number=> (
-                  <span key={number} onClick={()=> setPage(number)}>
+                  <span key={number} onClick={()=> setPage(number)}
+                  className={page==number ? 'active__page' : ''}
+                  >
                     {number + 1}
                   </span>
                 ))}
