9226222abf12f5d9b4233de0aa1699a544f87dcd
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-01-09 09:53:43 +0200

    add form of information

--- frontend/src/components/Boobing/Booking.jsx
+++ frontend/src/components/Boobing/Booking.jsx
@@ -4,6 +4,8 @@ import { Form, FormGroup, ListGroup, ListGroupItem, Button } from 'reactstrap'
 
 const Booking = ({ tour, avgRating }) => {
   const { price, reviews } = tour
+
+  const handleChange = (e) => {}
   return (
     <div className="booking">
       <div className="booking__top d-flex align-items-center justify-content-between">
@@ -17,6 +19,45 @@ const Booking = ({ tour, avgRating }) => {
       </div>
 
       {/*======== boking form start =======*/}
+      <div className="booking__form">
+        <h5>Information</h5>
+        <Form className="booking__info-form">
+          <FormGroup>
+            <input
+              type="text"
+              placeholder="Full Name"
+              id="fullName"
+              required
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <input
+              type="number"
+              placeholder="Phone"
+              id="phone"
+              required
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup className="d-flex align-items-center gap-3">
+            <input
+              type="date"
+              placeholder=""
+              id="bookAt"
+              required
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              placeholder="Guest"
+              id="guestSize"
+              required
+              onChange={handleChange}
+            />
+          </FormGroup>
+        </Form>
+      </div>
       {/*======== boking form end=======*/}
     </div>
   )
