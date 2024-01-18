253a14a3bb4d97804dc1c82dfac24367b9afde37
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-01-18 12:49:05 +0200

    add hashing password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,30 @@
 import User from '../models/User.js'
-
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 // user registration
 export const register = async (req, res) => {
-  
-    try {
-        
-    } catch (error) {
-        
-    }
+  try {
+
+    //hashing password
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+      photo: req.body.photo,
+    })
+
+    await newUser.save()
+
+    res.status(200).json({ success: true, message: 'successfully created' })
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: 'failed to create. Try aigain' })
+  }
 }
 
 // user login
