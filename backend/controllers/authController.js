8003ece6ca4c8c9dc248bb86ec4077fe4a1e61af
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-01-18 12:11:32 +0200

    add auth.js fille

---
+++ backend/controllers/authController.js
@@ -0,0 +1,18 @@
+import User from '../models/User.js'
+
+
+// user registration
+export const register = async (req, res) => {
+  
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// user login
+export const login = async (req, res) => {
+  try {
+  } catch (error) {}
+}
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 
 import tourRoute from './routes/tours.js';
 import userRoute from './routes/users.js';
+import authRoute from './routes/auth.js'
 
 dotenv.config()
 const app = express()
@@ -30,8 +31,10 @@ const connect = async () => {
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use('/auth', authRoute)
 app.use('/tours', tourRoute);
-app.use('/users', userRoute)
+app.use('/users', userRoute);
+
 
 app.listen(port, () => {
   connect();
