0602595e90a73fd2f47fa801fc99d982573266b5
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-01-19 11:06:54 +0200

    add useFetch

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,12 @@
+import  { useState, useEffect } from 'react';
+
+const useFetch = ()=>{
+
+    const [data,setData] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    return
+}
+
+export default useFetch
